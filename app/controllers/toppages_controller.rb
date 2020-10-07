25f8e2f082c02ad0e6641a7ac4f8b1ab0ec3b7ed
Daichi.Takakusaki <takakusaki.ready@gmail.com>
2020-10-07 22:51:32 +0900

    add front page routes and controller and view
    フロントページ用のルーティングなど準備

---
+++ app/controllers/toppages_controller.rb
@@ -0,0 +1,6 @@
+class ToppagesController < ApplicationController
+  skip_before_action :require_login, only: [:index]
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'users#new'
+  root to: 'toppages#index'
   resources :users, only: %i[index create]
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
